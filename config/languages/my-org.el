94357a3f6d5a91d8dd2852f00663c5a72c22ef8e
Temir Umurzakov <temir@umurzakov.com>
2015-06-18 20:40:05 +0600

    Org

--- config/languages/my-org.el
+++ config/languages/my-org.el
@@ -11,27 +11,15 @@
   :config
   (progn
     (setq org-todo-keywords
-          (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
-                  (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "PHONE" "MEETING"))))
+          (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)" "CANCELLED(c)")
+                  )))
 
     (setq org-todo-keyword-faces
           (quote (("TODO" :foreground "red" :weight bold)
                   ("NEXT" :foreground "blue" :weight bold)
                   ("DONE" :foreground "forest green" :weight bold)
-                  ("WAITING" :foreground "orange" :weight bold)
-                  ("HOLD" :foreground "magenta" :weight bold)
                   ("CANCELLED" :foreground "forest green" :weight bold)
-                  ("MEETING" :foreground "forest green" :weight bold)
-                  ("PHONE" :foreground "forest green" :weight bold))))
-
-    (setq org-todo-state-tags-triggers
-          (quote (("CANCELLED" ("CANCELLED" . t))
-                  ("WAITING" ("WAITING" . t))
-                  ("HOLD" ("WAITING") ("HOLD" . t))
-                  (done ("WAITING") ("HOLD"))
-                  ("TODO" ("WAITING") ("CANCELLED") ("HOLD"))
-                  ("NEXT" ("WAITING") ("CANCELLED") ("HOLD"))
-                  ("DONE" ("WAITING") ("CANCELLED") ("HOLD")))))
+                  )))
 
     (setq org-default-notes-file "~/org/inbox.org")
 
@@ -55,6 +43,11 @@
     (defun org-project-open (path)
       "Open project with PATH"
       (funcall 'neotree-dir path))
+
+    (add-hook 'org-agenda-mode-hook
+              (lambda ()
+                (org-save-all-org-buffers)
+                ))
 
     )
   )
